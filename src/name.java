ce873a08ed14f6c26f0634f1ccd999deeec13072
alexei.zhernosek <alexei.zhernosek@gmail.com>
2022-02-09 21:38:59 +0300

    Initial commit

---
+++ src/name.java
@@ -0,0 +1,13 @@
+public class name {
+    public static void main(String[] args) {
+        // проверка положительное или отрицательное
+        int a = 6;
+        if (a > 0) {
+            System.out.println("Число положительное");
+        } else if (a == 0) {
+            System.out.println("Число равно нулю");
+        } else {
+            System.out.println("Число отрицательное");
+        }
+        }
+}
